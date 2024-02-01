74ca523ea91e7e1e07d034f424194da6745765bb
Abhisolanki16 <97236636+Abhisolanki16@users.noreply.github.com>
2024-02-01 13:16:04 +0530

    Usage of all datatypes in complex structure like class

---
+++ datatype_handling.dart
@@ -0,0 +1,17 @@
+
+class DataTypes{
+  int a = 23;
+  String s = "Hello World";
+  double d = 1.45;
+  Map m = {'name':'Abhinav','marks': 20};
+  List l = [1 , 5 , 'hiii'];
+}
+
+void main(){
+  DataTypes obj  = new DataTypes();
+  print(obj.a);
+  print(obj.s);
+  print(obj.d);
+  print(obj.m);
+  print(obj.l);
+}
