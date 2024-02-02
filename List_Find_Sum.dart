6aaf1e63461913cb9ecc933a120325389f6d1cd5
Abhisolanki16 <97236636+Abhisolanki16@users.noreply.github.com>
2024-02-02 11:14:04 +0530

    Sum_of_element_List

---
+++ List_Find_Sum.dart
@@ -0,0 +1,12 @@
+
+sumOfElements(List<String> args){
+  List<int> numbers = [];
+  for (int i = 0; i < args.length; i++) {
+    numbers.add(int.parse(args[i]));
+  }
+  int sum = 0;
+  for(int num in numbers){ /// num iterates in numbers and add its value in sum
+    sum+=num;
+  }
+  print("Sum of all elements in the list is : ${sum} ");
+}
